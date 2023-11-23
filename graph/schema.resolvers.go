ffaa88f5ec16b12947ca44ce31395a92ad32d37b
loribean <aureliadotlim@gmail.com>
2023-11-23 13:11:33 +0800

    mutations for login & refresh token

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -39,12 +39,30 @@ func (r *mutationResolver) CreateUser(ctx context.Context, input model.NewUser) 
 
 // Login is the resolver for the login field.
 func (r *mutationResolver) Login(ctx context.Context, input model.Login) (string, error) {
-	panic(fmt.Errorf("not implemented: Login - login"))
+	var user users.User
+	user.Username = input.Username
+	user.Password = input.Password
+	if user.Authenticate() {
+		token, err := jwt.GenerateToken(user.Username)
+		if err != nil {
+			return "", err
+		}
+		return token, nil
+	}
+	return "", &users.WrongUsernameOrPasswordError{}
 }
 
 // RefreshToken is the resolver for the refreshToken field.
 func (r *mutationResolver) RefreshToken(ctx context.Context, input model.RefreshTokenInput) (string, error) {
-	panic(fmt.Errorf("not implemented: RefreshToken - refreshToken"))
+	username, err := jwt.ParseToken(input.Token)
+	if err != nil {
+		return "", fmt.Errorf("access denied")
+	}
+	token, err := jwt.GenerateToken(username)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
 
 // Links is the resolver for the links field.
